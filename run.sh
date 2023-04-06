8329bdc54e9d2817b1f40b53f354349ade6e4a77
crimama <crimama2@gmail.com>
2023-04-06 06:06:00 +0000

    init

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,3 python -m torch.distributed.launch --nproc_per_node=2 --use_env Pretrain.py --config ./configs/Pretrain.yaml 
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env Pretrain.py --config ./configs/Pretrain.yaml 
+python -m torch.distributed.launch --nproc_per_node=2 --use_env Retrieval.py \
+    --config ./configs/Retrieval_coco.yaml --output_dir output/Retrieval_coco \
+    --checkpoint ./output/Pretrain/ALBEF.pth
