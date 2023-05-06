2bc16f6c29b0d9300fd85affe8f63591db13428c
crimama <crimama2@gmail.com>
2023-05-06 10:28:49 +0000

    commit

--- requirement.sh
+++ requirement.sh
@@ -1,3 +1,5 @@
+pip install nlpaug 
+pip install --upgrade huggingface_hub
 pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install transformers==4.8.1
 pip install timm==0.4.9
