c1f7f9b72c5638074b692f0795586608c86ab8ce
crimama <crimama2@gmail.com>
2023-07-05 05:37:37 +0000

    fix requirement to run

--- requirement.sh
+++ requirement.sh
@@ -1,22 +1,26 @@
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+# pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+# # pip install transformers==4.26.1
+# # pip install timm==0.4.9
+# # pip install opencv-python
+# # pip install ruamel.yaml 
+# pip install transformers==4.8.1 timm==0.4.9 wget opencv-python ruamel.yaml salesforce-lavis
+
+# apt-get install libglib2.0
+
+# git clone https://github.com/cocodataset/cocoapi.git
+# cd cocoapi/PythonAPI
+# make
+
+
+
+pip install --upgrade pip 
+pip install ruamel.yaml
 pip install transformers==4.26.1
-pip install timm==0.4.9
-pip install opencv-python
-pip install ruamel.yaml 
-
-
-apt-get update && apt-get install libgl1
-apt-get install libglib2.0
-
-git clone https://github.com/cocodataset/cocoapi.git
-cd cocoapi/PythonAPI
-make
-
-pip install sentencepiece
-pip install Wand
-pip install nlpaug==1.1.11
+pip install wandb 
+# Text perturbation 
 pip install nltk 
-pip install git+https://github.com/PrithivirajDamodaran/Styleformer.git
-
+pip install nlpaug==1.1.11
+pip install sentencepiece
+#CV2 
+apt-get update && apt-get install libgl1
 apt-get install libmagickwand-dev
-
