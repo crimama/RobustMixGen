c3225a52b609710bcdd1b3c41f9f4397e7e3e276
crimama <crimama2@gmail.com>
2023-07-06 08:23:02 +0000

    [feat] add load_model_retrieval

---
+++ models/factory.py
@@ -0,0 +1,37 @@
+import torch 
+from models.vit import interpolate_pos_embed
+from models.tokenization_bert import BertTokenizer
+
+def load_model_retrieval(args, config, device):
+    from models.model_retrieval import ALBEF 
+    
+    tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
+    model = ALBEF(config=config, text_encoder=args.text_encoder, tokenizer=tokenizer)
+    
+    if args.checkpoint:    
+        checkpoint = torch.load(args.checkpoint, map_location='cpu') 
+        state_dict = checkpoint['model']
+        
+        # reshape positional embedding to accomodate for image resolution change
+        pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder.pos_embed'],model.visual_encoder)         
+        state_dict['visual_encoder.pos_embed'] = pos_embed_reshaped
+        m_pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder_m.pos_embed'],model.visual_encoder_m)   
+        state_dict['visual_encoder_m.pos_embed'] = m_pos_embed_reshaped 
+        
+        for key in list(state_dict.keys()):
+            if 'bert' in key:
+                encoder_key = key.replace('bert.','')         
+                state_dict[encoder_key] = state_dict[key] 
+                del state_dict[key]                
+        msg = model.load_state_dict(state_dict,strict=False)  
+        
+        print('load checkpoint from %s'%args.checkpoint)
+        print(msg)          
+    
+    model = model.to(device)   
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module   
+        
+    return model, model_without_ddp, tokenizer
