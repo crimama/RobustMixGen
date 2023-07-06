e54bca07b6eb528adc73bd47a3c4d1fa47a9248d
crimama <crimama2@gmail.com>
2023-07-06 05:33:14 +0000

    [feat] main.py to run many task for one time

---
+++ main.py
@@ -0,0 +1,29 @@
+from arguments import parser 
+from pathlib import Path
+import os 
+import ruamel.yaml as yaml
+
+
+if __name__ == '__main__':
+    config = parser()
+    args = config.args
+    config['output_dir'] = args.output_dir 
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    yaml.dump(config, open(os.path.join(args.output_dir, 'config.yaml'), 'w'))   
+    
+    
+    
+    # Train 
+    if not args['evaluate']: # args['evaluate] : only Evaluation 
+        main = __import__(f'{config.TASK}').__dict__['main']
+        # main(args, config)    
+        
+    # Evaluation Image 
+    args['checkpoint'] = os.path.join(args['output_dir'],'checkpoint_best.pth')    
+    eval_image = __import__(f'{config.TASK}').__dict__['eval_image']
+    eval_image(args, config)
+    
+    # Evaluation Text 
+    eval_text = __import__(f'{config.TASK}').__dict__['eval_text']
+    eval_text(args, config)
+    
