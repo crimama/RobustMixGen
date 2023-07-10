0c841e22c260aee7953f06d5528a74475ec16a85
crimama <crimama2@gmail.com>
2023-07-10 14:17:12 +0000

    grouding model 추가

--- models/factory.py
+++ models/factory.py
@@ -127,3 +127,35 @@ def load_model_vqa(args, config, device):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module  
     return model, model_without_ddp, tokenizer
+
+
+def load_model_grounding(args, config, device):
+    from models.model_retrieval import ALBEF 
+    tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
+    model = ALBEF(config = config, text_encoder=args.text_encoder, tokenizer=tokenizer)
+    
+    if args.checkpoint:    
+        checkpoint = torch.load(args.checkpoint, map_location='cpu') 
+        state_dict = checkpoint['model']
+        pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder.pos_embed'],model.visual_encoder)         
+        state_dict['visual_encoder.pos_embed'] = pos_embed_reshaped
+        m_pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder_m.pos_embed'],model.visual_encoder_m)   
+        state_dict['visual_encoder_m.pos_embed'] = m_pos_embed_reshaped 
+        
+        for key in list(state_dict.keys()):
+            if 'bert' in key:
+                encoder_key = key.replace('bert.','')         
+                state_dict[encoder_key] = state_dict[key] 
+                del state_dict[key]                
+        msg = model.load_state_dict(state_dict,strict=False)  
+        
+        print('load checkpoint from %s'%args.checkpoint)
+        print(msg)          
+    
+    model = model.to(device)   
+    
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module
+    return model, model_without_ddp, tokenizer
