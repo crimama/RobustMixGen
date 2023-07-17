2740ffcd38bd05d05d68a9acb95ad0c8236a28ee
crimama <crimama2@gmail.com>
2023-07-17 05:04:37 +0000

    [feat] add NLVR implementation

--- models/factory.py
+++ models/factory.py
@@ -159,3 +159,33 @@ def load_model_grounding(args, config, device):
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
     return model, model_without_ddp, tokenizer
+
+def load_model_nlvr(args, config, device):
+    from models.model_nlvr import ALBEF
+    tokenizer = BertTokenizer.from_pretrained(args.text_encoder)
+    
+    model = ALBEF(config=config, text_encoder=args.text_encoder, tokenizer=tokenizer)
+    
+    
+    if args.checkpoint:    
+        checkpoint = torch.load(args.checkpoint, map_location='cpu') 
+        state_dict = checkpoint['model']
+        
+        # reshape positional embedding to accomodate for image resolution change
+        pos_embed_reshaped = interpolate_pos_embed(state_dict['visual_encoder.pos_embed'],model.visual_encoder)         
+        state_dict['visual_encoder.pos_embed'] = pos_embed_reshaped
+        
+        if config['distill']:
+            model.copy_params()
+            
+        msg = model.load_state_dict(state_dict,strict=False)
+        print('load checkpoint from %s'%args.checkpoint)
+        print(msg)
+
+    model = model.to(device)   
+    
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model_without_ddp = model.module    
+    return model, model_without_ddp, tokenizer
