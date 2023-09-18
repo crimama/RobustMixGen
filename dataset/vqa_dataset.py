eb5b810e03e29f8d2b6731944b55b13c818c6866
crimama <crimama2@gmail.com>
2023-09-18 05:26:51 +0000

    [refactor] back to origin

--- dataset/vqa_dataset.py
+++ dataset/vqa_dataset.py
@@ -9,10 +9,7 @@ from dataset.utils import pre_question, pertur_check, get_transform
 
 
 class vqa_dataset(Dataset):
-    def __init__(self, ann_file, transform, vqa_root, vg_root, 
-                romixgen: object, romixgen_true: bool = True ,romixgen_prob: float = 0.1,
-                eos='[SEP]', split="train", max_ques_words=30, answer_list=''):
-        
+    def __init__(self, ann_file, transform, vqa_root, vg_root, eos='[SEP]', split="train", max_ques_words=30, answer_list=''):
         self.split = split        
         self.ann = []
         for f in ann_file:
@@ -24,10 +21,6 @@ class vqa_dataset(Dataset):
         self.max_ques_words = max_ques_words
         self.eos = eos
         
-        self.romixgen = romixgen
-        self.romixgen_true = romixgen_true
-        self.romixgen_prob = romixgen_prob 
-        
         if split=='test':
             self.max_ques_words = 50 # do not limit question length during test
             self.answer_list = json.load(open(answer_list,'r'))    
@@ -36,25 +29,30 @@ class vqa_dataset(Dataset):
     def __len__(self):
         return len(self.ann)
     
-    def __load_image__(self,ann):
+    def __getitem__(self, index):    
+        
+        ann = self.ann[index]
+        
         if ann['dataset']=='vqa':
             image_path = os.path.join(self.vqa_root,ann['image'])    
         elif ann['dataset']=='vg':
-            image_path = os.path.join(self.vg_root,ann['image']) 
-        image = Image.open(image_path).convert('RGB')  
-        image = self.transform(image) 
-        return image 
-    
-    def __test_load__(self, ann):
-        image = self.__load_image__(ann)
-        question = pre_question(ann['question'],self.max_ques_words)   
-        question_id = ann['question_id']
-        return image, question, question_id 
-    
-    def __train_load__(self, ann):
-        image = self.__load_image__(ann)
-        question = pre_question(ann['question'],self.max_ques_words)   
-        if ann['dataset']=='vqa':                
+            image_path = os.path.join(self.vg_root,ann['image'])  
+            
+        image = Image.open(image_path).convert('RGB')   
+        image = self.transform(image)          
+        
+        if self.split == 'test':
+            question = pre_question(ann['question'],self.max_ques_words)   
+            question_id = ann['question_id']            
+            return image, question, question_id
+
+
+        elif self.split=='train':                       
+            
+            question = pre_question(ann['question'],self.max_ques_words)        
+            
+            if ann['dataset']=='vqa':
+                
                 answer_weight = {}
                 for answer in ann['answer']:
                     if answer in answer_weight.keys():
@@ -65,26 +63,13 @@ class vqa_dataset(Dataset):
                 answers = list(answer_weight.keys())
                 weights = list(answer_weight.values())
 
-        elif ann['dataset']=='vg':
-            answers = [ann['answer']]
-            weights = [0.5]  
-        answers = [answer+self.eos for answer in answers]
-            
-        return image, question, answers, weights
-    
-    def __getitem__(self, index):    
-        ann = self.ann[index]
+            elif ann['dataset']=='vg':
+                answers = [ann['answer']]
+                weights = [0.5]  
 
-        if self.split == 'test':            
-            image, question, question_id = self.__test_load__(ann)            
-            return image, question, question_id 
-        
-        elif self.split == 'train':
-            if (self.romixgen_true) & (random.random() < self.romixgen_prob):
-                image, question, answers, weights = self.romixgen(ann)
-            else:
-                image, question, answers, weights = self.__train_load__(ann)    
-            return image, question, answers, weights 
+            answers = [answer+self.eos for answer in answers]
+                
+            return image, question, answers, weights
 
 
 class vqa_pertur_dataset(vqa_dataset):
